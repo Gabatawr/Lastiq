97c25e03255267849838cfdce03b86529e4ce420
Artem Gabatawr <gabatawr@gmail.com>
2021-04-12 11:17:33 +0300

    StickModel

---
+++ Lastiq/Models/StickContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lastiq.Models
+{
+    public interface IStickContent { }
+
+    [Serializable]
+    public class CheckboxContent : IStickContent
+    {
+        public string Text { get; set; } = string.Empty;
+        public bool IsChecked { get; set; }
+    }
+
+    [Serializable]
+    public class TextContent : IStickContent
+    {
+        public string Text { get; set; } = string.Empty;
+    }
+}
--- Lastiq/Models/StickModel.cs
+++ Lastiq/Models/StickModel.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Lastiq.Models
 {
+    [Serializable]
     public class StickModel
     {
-        public bool Completed { get; set; }
+        //---------------------------------------------------------------------
+        // Main info
+        public int Id { get; set; }
+        public int CreatorId { get; set; }
+        public List<int> FriendsId { get; set; }
+            = new List<int>();
+        //---------------------------------------------------------------------
+        // Contents
+        public string Title { get; set; }
+        public List<IStickContent> Contents { get; set; }
+            = new List<IStickContent>();
+        public List<string> Tags { get; set; }
+            = new List<string>();
+        public DateTime DateTime { get; set; }
+            = DateTime.MaxValue;
+        //---------------------------------------------------------------------
+        // View
+        public SolidColorBrush Color { get; set; }
+            = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE600")) { Opacity = 0.64 };
+        //---------------------------------------------------------------------
+        public StickModel(int id, int creatorId)
+        {
+            Id = id;
+            CreatorId = creatorId;
+        }
+        //---------------------------------------------------------------------
     }
 }
