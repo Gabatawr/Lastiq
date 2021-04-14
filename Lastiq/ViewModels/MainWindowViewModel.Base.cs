fb71f97b524f7bdd8ba01f4133034c9c2a44bb08
Gabatawr <gabatawr@gmail.com>
2021-04-14 18:20:04 +0300

    fix Base

--- Lastiq/ViewModels/MainWindowViewModel.Base.cs
+++ Lastiq/ViewModels/MainWindowViewModel.Base.cs
@@ -14,14 +14,6 @@ namespace Lastiq.ViewModels
             StickCollectionInit();
             //---------------------------------------------------------------------
 
-            #region TagListTest
-
-            TagCollection.Add(new TagModel("TagOne"));
-            TagCollection.Add(new TagModel("TagTwo"));
-            TagCollection.Add(new TagModel("TagThree"));
-
-            #endregion TagListTest
-
             #region StickListTest
 
             Random rand = new Random();
@@ -35,9 +27,13 @@ namespace Lastiq.ViewModels
                 Stick.Contents.Add(new TextContent($"Teeeee eeeee eeeeeee eeeeeeeeee eeeeeee eeeeeeeeee eeeeeeeeeeee eeeeeeeeeee eeeeeeeee ee eeext {i}"));
                 Stick.Contents.Add(new CheckboxContent($"Checkbox {i}"));
 
-                for (int t = 0; t < rand.Next(TagCollection.Count); t++)
+                Stick.Contents.Add(new TextContent($"Text two"));
+                Stick.Contents.Add(new CheckboxContent($"Checkbox two"));
+                Stick.Contents.Add(new CheckboxContent($"Checkbox three"));
+
+                for (int t = 0; t < rand.Next(5); t++)
                 {
-                    Stick.Tags.Add(TagCollection[t].Text);
+                    Stick.Tags.Add($"Tag {t}");
                 }
 
                 StickCollection.Add(new StickViewModel() { Stick = Stick });
